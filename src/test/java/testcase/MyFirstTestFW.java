a952dc5f00bc83c611d3c66d8e335bee9fea8f1a
macro5v <eduardyacenko@gmail.com>
2024-02-18 14:01:30 +0200

    Changelist: 1. Data Provider has been added.

--- src/test/java/testcase/MyFirstTestFW.java
+++ src/test/java/testcase/MyFirstTestFW.java
@@ -2,24 +2,32 @@ package testcase;
 
 import base.BaseTest;
 import org.openqa.selenium.By;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class MyFirstTestFW extends BaseTest {
-    @Test
-    public static void LoginTest() {
+    @Test (dataProvider = "testData")
+    public static void LoginTest(String username, String password) {
         System.out.println("Click on Sign in Button");
         driver.findElement(By.partialLinkText(locators.getProperty("signin_link"))).click();
 
         System.out.println("Enter email");
-        driver.findElement(By.id(locators.getProperty("email_field"))).sendKeys("eduardyacenko@gmail.com");
+        driver.findElement(By.id(locators.getProperty("email_field"))).sendKeys(username);
 
         System.out.println("Click on next button");
         driver.findElement(By.id(locators.getProperty("next_button"))).click();
 
         System.out.println("Enter password");
-        driver.findElement(By.xpath(locators.getProperty("pwd_field"))).sendKeys("Apriorit!1672");
+        driver.findElement(By.xpath(locators.getProperty("pwd_field"))).sendKeys(password);
 
         System.out.println("Click on Next button");
         driver.findElement(By.xpath(locators.getProperty("login_next_button"))).click();
+    }
+
+    @DataProvider(name = "testData")
+    public Object[][] tData() {
+        return new Object[][]{
+                {"eduardyacenko@gmail.com", "Apriorit!1672"}
+        };
     }
 }
