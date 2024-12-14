415c472c7123d4bc3a55fabb54cfd49845059c91
eyatsenko <eduardyacenko@gmail.com>
2024-12-14 22:05:25 +0200

    1. Some minor improvements

--- src/main/java/com/example/page/demoqa/components/FormModalWindow.java
+++ src/main/java/com/example/page/demoqa/components/FormModalWindow.java
@@ -4,6 +4,7 @@ import com.example.page.AbstractPageObject;
 import io.qameta.allure.Step;
 import lombok.Getter;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
@@ -27,15 +28,29 @@ public class FormModalWindow extends AbstractPageObject {
     @Step("Check that {key} is: {value}")
     public FormModalWindow verifyModalResult(String key, String value) {
         WebElement parentElement = modalTable.findElement(By.xpath(".//*[text()='" + key + "']/parent::*"));
-        String values = parentElement.getText().replace(key,"").trim();
+        String values = parentElement.getText().replace(key, "").trim();
         Assert.assertEquals(values, value);
         return this;
     }
 
     @Step("Close modal window")
-    public FormModalWindow clickCloseModalButton() {
+    public FormModalWindow closeModalWindow() {
         waitUtils.waitForElementToBeClickable(closeModalButton);
         closeModalButton.click();
         return this;
+    }
+
+    @Step("Check that Modal Window is displayed")
+    public void checkThatModalWindowIsDisplayed() {
+        Assert.assertTrue(isModalTableDisplayed());
+    }
+
+    @Step("Check that Modal Window is not displayed")
+    public void checkThatModalWindowIsNotDisplayed() {
+        try {
+            modalTable.isDisplayed();
+        } catch (NoSuchElementException e) {
+            Assert.assertTrue(true);
+        }
     }
 }
--- src/test/java/com/example/testcase/demoqa/FormsTests.java
+++ src/test/java/com/example/testcase/demoqa/FormsTests.java
@@ -4,14 +4,13 @@ import com.example.base.BaseTest;
 import com.example.models.User;
 import com.example.page.demoqa.MainPage;
 import com.example.page.demoqa.PracticeFormPage;
-import com.example.page.demoqa.components.CalendarComponent;
 import com.example.page.demoqa.components.FormModalWindow;
 import com.example.page.demoqa.components.FormsSidebarMenu;
 import com.example.utilities.RandomDataUtils;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.nio.file.Paths;
 import java.text.ParseException;
 
 public class FormsTests extends BaseTest {
@@ -20,7 +19,6 @@ public class FormsTests extends BaseTest {
     private FormModalWindow formModalWindow;
     private FormsSidebarMenu formsSidebarMenu;
     private User user;
-    private CalendarComponent calendar;
 
     @BeforeMethod
     public void setUp() {
@@ -31,29 +29,27 @@ public class FormsTests extends BaseTest {
         user = RandomDataUtils.getRandomUser();
     }
 
-    @Test(description = "Check form filling")
-    public void FillFormTest() throws ParseException {
+    @Test(description = "Check form filling with all valid data", priority = 1)
+    public void fillAllFormTest() throws ParseException {
         mainPage.clickFormsCard();
 
         formsSidebarMenu.clickPracticeFormMenuItem();
 
         formPage.fillFirstName(user.getFirstName())
                 .fillLastName(user.getLastName())
-                .fillEmailField(user.getEmail())
+                .fillEmail(user.getEmail())
                 .selectGender(user.getGender())
-                .fillNumberField(user.getMobileNumber())
-                .clickOnCalendar().setDate(12, "May", 2015);
-
-        formPage.fillSubjects(user.getSubjects())
+                .fillNumber(user.getMobileNumber())
+                .setDateOfBirth(12, "May", 2015)
+                .fillSubjects(user.getSubjects())
                 .selectHobbies(user.getHobbies())
                 .uploadPicture(user.getPicture())
                 .fillCurrentAddress(user.getCurrentAddress())
                 .selectState(user.getState())
                 .selectCity(user.getCity())
-                .clickSubmitButton();
+                .submitForm();
 
-        logAndStep("Check that Modal Window is displayed");
-        Assert.assertTrue(formModalWindow.isModalTableDisplayed());
+        formModalWindow.checkThatModalWindowIsDisplayed();
 
         formModalWindow.verifyModalResult("Student Name", user.getFirstName() + " " + user.getLastName());
         formModalWindow.verifyModalResult("Student Email", user.getEmail());
@@ -61,9 +57,109 @@ public class FormsTests extends BaseTest {
         formModalWindow.verifyModalResult("Mobile", user.getMobileNumber());
         formModalWindow.verifyModalResult("Date of Birth", "12 May,2015");
         formModalWindow.verifyModalResult("Subjects", (String.join(", ", user.getSubjects())));
+        formModalWindow.verifyModalResult("Hobbies", (String.join(", ", user.getHobbies())));
+        formModalWindow.verifyModalResult("Picture", Paths.get(user.getPicture()).getFileName().toString());
         formModalWindow.verifyModalResult("Address", user.getCurrentAddress());
         formModalWindow.verifyModalResult("State and City", user.getState() + " " + user.getCity());
 
-        formModalWindow.clickCloseModalButton();
+        formModalWindow.closeModalWindow();
+    }
+
+    @Test(description = "Check form filling with only required fields", priority = 2)
+    public void fillRequiredFieldsFormTest() throws ParseException {
+        mainPage.clickFormsCard();
+
+        formsSidebarMenu.clickPracticeFormMenuItem();
+
+        formPage.fillFirstName(user.getFirstName())
+                .fillLastName(user.getLastName())
+                .selectGender(user.getGender())
+                .fillNumber(user.getMobileNumber())
+                .setDateOfBirth(12, "May", 2015)
+                .submitForm();
+
+        formModalWindow.checkThatModalWindowIsDisplayed();
+
+        formModalWindow.verifyModalResult("Student Name", user.getFirstName() + " " + user.getLastName());
+        formModalWindow.verifyModalResult("Student Email", "");
+        formModalWindow.verifyModalResult("Gender", user.getGender());
+        formModalWindow.verifyModalResult("Mobile", user.getMobileNumber());
+        formModalWindow.verifyModalResult("Date of Birth", "12 May,2015");
+        formModalWindow.verifyModalResult("Subjects", "");
+        formModalWindow.verifyModalResult("Hobbies", "");
+        formModalWindow.verifyModalResult("Picture", "");
+        formModalWindow.verifyModalResult("Address", "");
+        formModalWindow.verifyModalResult("State and City", "");
+
+        formModalWindow.closeModalWindow();
+    }
+
+    @Test(description = "Check form filling with missing First Name", priority = 3)
+    public void fillAllFormMissingFirstNameTest() throws ParseException {
+        mainPage.clickFormsCard();
+
+        formsSidebarMenu.clickPracticeFormMenuItem();
+
+        formPage.fillLastName(user.getLastName())
+                .fillEmail(user.getEmail())
+                .selectGender(user.getGender())
+                .fillNumber(user.getMobileNumber())
+                .setDateOfBirth(12, "May", 2015)
+                .fillSubjects(user.getSubjects())
+                .selectHobbies(user.getHobbies())
+                .uploadPicture(user.getPicture())
+                .fillCurrentAddress(user.getCurrentAddress())
+                .selectState(user.getState())
+                .selectCity(user.getCity())
+                .submitForm();
+
+        formModalWindow.checkThatModalWindowIsNotDisplayed();
+        formPage.checkThatFirstNameFieldIsHighlightedInRed();
+    }
+
+    @Test(description = "Check form filling with missing Last Name", priority = 4)
+    public void fillAllFormMissingLastNameTest() throws ParseException {
+        mainPage.clickFormsCard();
+
+        formsSidebarMenu.clickPracticeFormMenuItem();
+
+        formPage.fillFirstName(user.getFirstName())
+                .fillEmail(user.getEmail())
+                .selectGender(user.getGender())
+                .fillNumber(user.getMobileNumber())
+                .setDateOfBirth(12, "May", 2015)
+                .fillSubjects(user.getSubjects())
+                .selectHobbies(user.getHobbies())
+                .uploadPicture(user.getPicture())
+                .fillCurrentAddress(user.getCurrentAddress())
+                .selectState(user.getState())
+                .selectCity(user.getCity())
+                .submitForm();
+
+        formModalWindow.checkThatModalWindowIsNotDisplayed();
+        formPage.checkThatLastNameFieldIsHighlightedInRed();
+    }
+
+    @Test(description = "Check form filling with missing Phone Number", priority = 5)
+    public void fillAllFormMissingPhoneNumberTest() throws ParseException {
+        mainPage.clickFormsCard();
+
+        formsSidebarMenu.clickPracticeFormMenuItem();
+
+        formPage.fillFirstName(user.getFirstName())
+                .fillLastName(user.getLastName())
+                .fillEmail(user.getEmail())
+                .selectGender(user.getGender())
+                .setDateOfBirth(12, "May", 2015)
+                .fillSubjects(user.getSubjects())
+                .selectHobbies(user.getHobbies())
+                .uploadPicture(user.getPicture())
+                .fillCurrentAddress(user.getCurrentAddress())
+                .selectState(user.getState())
+                .selectCity(user.getCity())
+                .submitForm();
+
+        formModalWindow.checkThatModalWindowIsNotDisplayed();
+        formPage.checkThatPhoneNumberFieldIsHighlightedInRed();
     }
 }
